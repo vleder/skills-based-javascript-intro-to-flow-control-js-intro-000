42565cb2edb23bfa17f7cd15937667e92c8046ef
Vedran Leder <gothic_0013@yahoo.co.uk>
2019-10-05 21:54:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,7 +7,7 @@ function basicTeenager(age) {
 
 function teenager(age) {
   if(age >= 13 && age <= 19) {
-    return 'You are a teenager'
+    return 'You are a teenager!'
   } 
   return "You are not a teenager"
 
