9964396b8ffc2bf2e1235b745d6547df85cfca16
Vedran Leder <gothic_0013@yahoo.co.uk>
2019-10-05 21:52:41 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,7 +30,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {   
   if(age >= 13 && age <= 19) {
-    return 'You are a teenager!'
+    return 'You are a teenager'
   } 
   return "You have an age"
 
