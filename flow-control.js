e7f71be3e0798fdb1ffeedd1dbd138e9920eca09
Vedran Leder <gothic_0013@yahoo.co.uk>
2019-10-05 21:52:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,7 +25,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  if(age >= 13 && age <= 19) ? 'You are a teenager' : "You are not a teenager!"
+  return (age >= 13 && age <= 19) ? 'You are a teenager' : "You are not a teenager!"
 }
 
 function switchAge(age) {   
